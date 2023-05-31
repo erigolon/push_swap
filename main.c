4466b15ec46134be4526096fef50964da5444f67
Esteban Rigolon <estebanrigolon@gmail.com>
2023-05-31 14:25:42 +0200

    Borrado de libft y arreglo del push_swap
    La libreria libft no me ahce falta, asi que se borra, y el fallo que
    tenía con la entrada de un argumento ya no da problemas.

--- main.c
+++ main.c
@@ -6,11 +6,21 @@
 /*   By: erigolon <erigolon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 15:03:37 by erigolon          #+#    #+#             */
-/*   Updated: 2023/05/31 13:02:55 by erigolon         ###   ########.fr       */
+/*   Updated: 2023/05/31 14:09:41 by erigolon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./includes/push_swap.h"
+
+int	len_str(char **str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
 
 int	main(int argc, char **argv)
 {
@@ -21,16 +31,13 @@ int	main(int argc, char **argv)
 	if (argc == 1)
 		return (0);
 	stack_b = NULL;
+
 	if (argc == 2)
 	{
 		str = ft_split(argv[1], ' ');
-		printf("%sCOSA\n", str[0]);
-		printf("%sCOSA\n", str[1]);
-		printf("%sCOSA\n", str[2]);
 		if (!checkers(str, 0))
 			return (free(str), 0);
-		stack_a = add_to_list(ft_strlen(*str), str, 0);
-		printf("%zuCOSANUMERO\n", ft_strlen(*str));
+		stack_a = add_to_list(len_str(str), str, 0);
 		free(str);
 	}
 	else if (argc > 2)
--- moves.c
+++ moves.c
@@ -6,7 +6,7 @@
 /*   By: erigolon <erigolon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 11:52:38 by erigolon          #+#    #+#             */
-/*   Updated: 2023/05/31 13:03:56 by erigolon         ###   ########.fr       */
+/*   Updated: 2023/05/31 14:25:12 by erigolon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,11 +29,11 @@ void	swap(t_stack *stack)
 void	do_sa(t_stack **stack_a)
 {
 	swap(*stack_a);
-	printf("sa\n");
+	ft_putstr_fd("sa\n", 1);
 }
 
 void	do_sb(t_stack **stack_b)
 {
 	swap(*stack_b);
-	printf("sb\n");
+	ft_putstr_fd("sb\n", 1);
 }
